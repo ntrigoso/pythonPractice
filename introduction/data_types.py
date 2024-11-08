ae48e54c1b27f1fead892c8a7262468fe35e4ae0
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-08 17:56:26 -0500

    Add files via upload

---
+++ introduction/data_types.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# data type- string 
+"Hello, World!"
+
+# data type- float
+2.2 + 2.2 
+
+# data type- boolean
+True
+
+# data type- NoneType: represent absence of value 
+None
+
+
+# In[ ]:
+
+
+
+
