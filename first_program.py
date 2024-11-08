518de52cf6c32dcea95db736c32739d053c51dd5
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-08 17:47:37 -0500

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# first prgram practice writing "hello world"
+print("Hello, World!")
+
+
+# In[2]:
+
+
+# more practice 
+print("I studied computer science at Georgia State")
+print("My name is")
+
+
+# In[ ]:
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def print_hello():
+    print("Hello")
+
+
+# In[ ]:
+
+
+
+
