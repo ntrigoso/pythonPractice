f0ebcbd2c0a168e91801d13a245078ab6c276d45
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 17:20:01 -0500

    Add files via upload

---
+++ introduction/logical_operators/or.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+# all statements have to be false for the entire comparison to be false 
+
+print(1 == 1 or 1 == 2)
+print(1 == 2 or 2 == 1)
+print(1 == 1 or 1 == 2 or 1 == 3)
+
