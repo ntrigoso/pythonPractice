714bcf86026968be010aa71754238081726f8760
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 16:23:03 -0500

    Add files via upload

---
+++ introduction/constants_varibles .py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# creating a variable
+
+b = 100 
+
+
+# In[1]:
+
+
+# changing the value of a variable 
+
+x = 100 
+print(x) 
+
+x = 200 
+print(x)
+
+
+# In[2]:
+
+
+# arithmetic operations with variables 
+
+x = 10 
+y = 10 
+z = x + y
+print(z) 
+a = x - y 
+print(a) 
+
+
+# In[ ]:
+
+
+# variables storing other data type
+
+hi = "Hello, World!"
+my_float = 2.2
+my_boolean = True 
+
