036799757165f9da47ee9a444a4f6a81041f9e1d
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 16:32:56 -0500

    Add files via upload

---
+++ introduction/conditional_statements/elif_statement.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+# elif statements 
+home = "Thailand"
+if home == "Japan":
+    print("Hello, Japan!")
+elif home == "Thailand":
+    print("Hello, Thailand!")
+else:
+    print("Hello, World!")
+
