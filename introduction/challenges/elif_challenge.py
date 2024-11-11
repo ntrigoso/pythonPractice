8578faa96448e1c602e1f30ef901111f572e03d6
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 16:38:33 -0500

    Add files via upload

---
+++ introduction/challenges/elif_challenge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+x = 10 
+if x <= 10:
+    print("x is less than or equal to 10")
+elif 10 < x <= 25:
+    print("x is greater than 10 but less than or equal to 25")
+else:
+    print("x is greater than 25")
+print("\n")
+
+
