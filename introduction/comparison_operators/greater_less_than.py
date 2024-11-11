4abee5f8b770062e8635f4a564e882544c15fd19
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 17:05:26 -0500

    Create greater_less_than.py

---
+++ introduction/comparison_operators/greater_less_than.py
@@ -0,0 +1,14 @@
+# returns either True or False
+
+
+# greater than
+print(100 > 10)
+
+# less than
+print(100 < 10)
+
+# greater than or equal to
+print(2 >= 2)
+
+# less than or equal to
+print(2 <= 2)
