d520e3c6cd4b2261a54387b4b1d8f105e8a08071
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 16:40:10 -0500

    Update if_else_challenge.py

--- introduction/challenges/if_else_challenge.py
+++ introduction/challenges/if_else_challenge.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
-
+# write a program that prints a message if a variable is less than 10, and a different message if it is greater than or equal to 10
 
 x = 10 
 if x < 10: 
     print("x is less than 10")
 else:
     print("x is greater than or equal to 10")
-print("\n")
 
