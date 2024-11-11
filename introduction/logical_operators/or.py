0b8714d89274cf6a818530fc26a986f22a19ad76
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 17:21:08 -0500

    Update or.py

--- introduction/logical_operators/or.py
+++ introduction/logical_operators/or.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# all statements have to be false for the entire comparison to be false 
+# all statements have to be false for the entire statement to be false 
 
 print(1 == 1 or 1 == 2)
 print(1 == 2 or 2 == 1)
