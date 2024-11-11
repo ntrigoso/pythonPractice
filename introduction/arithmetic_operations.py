3c5e4171cb12567a0e18d2bf3fad671a63329fb1
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 16:28:07 -0500

    Add files via upload

---
+++ introduction/arithmetic_operations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Integer division / floored quotient: returns quotient 
+
+13 // 5 
+
+
+# In[5]:
+
+
+# Modulo / remainder 
+
+13 % 5
+
+
+# In[6]:
+
+
+# Division: returns result as a floating point number 
+
+14 / 3
+
