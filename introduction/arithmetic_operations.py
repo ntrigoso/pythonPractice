c20dae918614799c6297a7af3ee2f949d4a52a0e
ntrigoso <82123159+ntrigoso@users.noreply.github.com>
2024-11-11 16:28:40 -0500

    Update arithmetic_operations.py

--- introduction/arithmetic_operations.py
+++ introduction/arithmetic_operations.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
 
 # Integer division / floored quotient: returns quotient 
 
 13 // 5 
 
 
-# In[5]:
-
-
 # Modulo / remainder 
 
 13 % 5
-
-
-# In[6]:
 
 
 # Division: returns result as a floating point number 
